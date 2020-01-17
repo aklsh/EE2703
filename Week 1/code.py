1e52da1ef361c4eabbc6bebfc59fc6a0be9215c3
Akilesh Kannan <akileshkannan@gmail.com>
2020-01-17 19:53:08 +0530

    week 1 finished ig

--- Week 1/code.py
+++ Week 1/code.py
@@ -14,17 +14,47 @@ CIRCUIT_END = ".end"
 RESISTOR = "R"
 CAPACITOR = "C"
 INDUCTOR = "L"
+IVS = "V"
+ICS = "I"
+VCVS = "E"
+VCCS = "G"
+CCVS = "H"
+CCCS = "F"
 
 # Extracting the tokens from a Line
 def line2tokens(spiceLine):
     tokens = []
     allWords = spiceLine.split()
-    if(len(allWords)==4):
+
+    # R, L, C, Independent Sources
+    if(len(allWords) == 4):
         elementName = allWords[0]
         node1 = allWords[1]
         node2 = allWords[2]
         value = allWords[3]
-    return [elementName, node1, node2, value]
+        return [elementName, node1, node2, value]
+
+    # CCxS
+    elif(len(allWords) == 5):
+        elementName = allWords[0]
+        node1 = allWords[1]
+        node2 = allWords[2]
+        voltageSource = allWords[3]
+        value = allWords[4]
+        return [elementName, node1, node2, voltageSource, value]
+
+    # VCxS
+    elif(len(allWords) == 6):
+        elementName = allWords[0]
+        node1 = allWords[1]
+        node2 = allWords[2]
+        voltageSourceNode1 = allWords[3]
+        voltageSourceNode2 = allWords[4]
+        value = allWords[5]
+        return [elementName, node1, node2, voltageSourceNode1, voltageSourceNode2, value]
+
+    else:
+        return []
 
 if __name__ == "__main__":
 
@@ -50,8 +80,10 @@ if __name__ == "__main__":
 
                         SPICELinesActual = SPICELines[identifier1+1:identifier2]
                         SPICELinesTokens = [line2tokens(line) for line in SPICELinesActual]
-                        for x in SPICELinesTokens:
-                            print(x)
+                        for x in SPICELinesTokens[::-1]:
+                            for y in x[::-1]:
+                                print(y, end=' ')
+                            print('')
                     except ValueError:
                         print("Netlist does not abide to given format!")
         except FileNotFoundError:
