5fd583777f3064efdcbd9e6f7b93f544242517ee
Akilesh Kannan <akileshkannan@gmail.com>
2020-01-19 09:47:04 +0530

    have to start this

---
+++ Week 3/code.py
@@ -0,0 +1,9 @@
+'''
+------------------------------------
+Assignment 3 - EE2703 (Jan-May 2020)
+Done by Akilesh Kannan (EE18B122)
+Created on 18/01/20
+Last Modified on 18/01/20
+------------------------------------
+'''
+import numpy as np
