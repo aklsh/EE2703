803d99c75d8dc3ee72c7ce91dcfbe203946ba1a7
Akilesh Kannan <akileshkannan@gmail.com>
2020-03-13 19:18:06 +0530

    fin till q3

--- Week 7/code_.py
+++ Week 7/code_.py
@@ -3,7 +3,7 @@
 Assignment 7 - EE2703 (Jan-May 2020)
 Done by Akilesh Kannan (EE18B122)
 Created on 12/03/20
-Last Modified on 12/03/20
+Last Modified on 13/03/20
 ------------------------------------
 '''
 
@@ -14,6 +14,10 @@ import sympy as sym
 
 PI = np.pi
 s = sym.symbols('s')
+plotsDir='plots/'
+
+plt.rcParams['figure.figsize'] = 18, 6
+plt.rcParams['font.family'] = "sans"
 
 def lowPass(R1, R2, C1, C2, G, Vi):
     s = sym.symbols('s')
@@ -26,10 +30,10 @@ def lowPass(R1, R2, C1, C2, G, Vi):
                     0,
                     -Vi/R1])
     V = A.inv() * b
-    return (A, b, V[3])
+    return V[3]
 
 def sympyToLTI(symboFunc):
-    numer, denom = sym.simplify(symboFunc).as_numer_denom()
+    numer, denom = symboFunc.as_numer_denom()
     numer = sym.Poly(numer, s)
     denom = sym.Poly(denom, s)
     numeratorCoeffs = numer.all_coeffs()
@@ -43,25 +47,57 @@ def sympyToLTI(symboFunc):
     return numeratorCoeffs, denominatorCoeffs
 
 # Question 1
-A, b, Vo = lowPass(10000, 10000, 1e-9, 1e-9, 1.586, 1)
+Vo = lowPass(10000, 10000, 1e-9, 1e-9, 1.586, 1)
 voNumerator, voDenominator = sympyToLTI(Vo)
 ckt1 = sgnl.lti(voNumerator, voDenominator)
-# Calculate step response
-t, voStep = sgnl.step(ckt1, None, np.linspace(0, 0.001, 10000))
+
+## Bode Plot of Transfer Function
+w, mag, phase = sgnl.bode(ckt1, w=np.linspace(1, 1e6, 1e6))
+fig1 = plt.figure(1)
+fig1.suptitle(r'Bode Plot of Transfer function of lowpass filter')
+plt.subplot(211)
+plt.semilogx(w, mag)
+plt.ylabel(r'$20log(\|H(j\omega)\|)$')
+plt.subplot(212)
+plt.semilogx(w, phase)
+plt.xlabel(r'$\omega \ \to$')
+plt.ylabel(r'$\angle H(j\omega)$')
+plt.savefig(plotsDir+'Fig 1.png')
+
+## Calculate step response
+t, voStep = sgnl.step(ckt1, None, np.linspace(0, 0.1, 1000000))
+plt.figure(2)
 plt.plot(t, voStep)
+plt.title(r'Step Response of Lowpass filter')
 plt.xlabel(r'$t\ \to$')
 plt.ylabel(r'$V_o(t)\ \to$')
+plt.xlim(0, 1e-3)
 plt.grid(True)
-plt.show(block=False)
-
+plt.savefig(plotsDir+'Fig 2.png')
 
 # Question 2
 vi = np.heaviside(t,1)*(np.sin(2e3*PI*t)+np.cos(2e6*PI*t))
 
-plt.figure(2)
+plt.figure(3)
 plt.plot(t, vi)
+plt.title(r'$V_i(t)=(sin(2x10^3\pi t)+cos(2x10^6\pi t))u(t)$ to Lowpass filter')
 plt.xlabel(r'$t\ \to$')
 plt.ylabel(r'$V_i(t)\ \to$')
+plt.xlim(0, 1e-3)
+plt.grid(True)
+plt.savefig(plotsDir+'Fig 3.png')
+
+time, vOut, rest = sgnl.lsim(ckt1, vi, np.linspace(0, 1e-1, 1e6))
+plt.figure(4)
+plt.plot(time, vOut)
+plt.title(r'$V_o(t)$ for $V_i(t)=(sin(2x10^3\pi t)+cos(2x10^6\pi t))u(t)$ for Lowpass filter')
+plt.xlabel(r'$t\ \to$')
+plt.ylabel(r'$V_o(t)\ \to$')
+plt.xlim(0, 1e-3)
+plt.grid(True)
+plt.savefig(plotsDir+'Fig 4.png')
+
+# Question 3
 def highPass(R1, R3, C1, C2, G, Vi):
     s = sym.symbols('s')
     A = sym.Matrix([[0, -1, 0, 1/G],
@@ -75,13 +111,53 @@ def highPass(R1, R3, C1, C2, G, Vi):
                     -Vi*s*C1])
     return (A.inv()*b)[3]
 
-Vo = highPass(1e4, 1e4, 1e-9, 1e-9, 1.586, 1)
+Vo = highPass(10000, 10000, 1e-9, 1e-9, 1.586, 1)
 voNumerator, voDenominator = sympyToLTI(Vo)
 ckt2 = sgnl.lti(voNumerator, voDenominator)
-time, vOut, rest = sgnl.lsim(ckt2, vi, np.linspace(0, 1e-5, 1e4))
-plt.figure(3)
-plt.plot(time, vOut)
+
+## Bode Plot of Transfer Function
+w, mag, phase = sgnl.bode(ckt2, w=np.linspace(1, 1e6, 1e6))
+fig5 = plt.figure(5)
+fig5.suptitle(r'Bode Plot of Transfer function of highpass filter')
+plt.subplot(211)
+plt.semilogx(w, mag)
+plt.ylabel(r'$20log(\|H(j\omega)\|)$')
+plt.subplot(212)
+plt.semilogx(w, phase)
+plt.xlabel(r'$\omega \ \to$')
+plt.ylabel(r'$\angle H(j\omega)$')
+plt.savefig(plotsDir+'Fig 5.png')
+
+## Calculate step response
+t, voStep = sgnl.step(ckt2, None, np.linspace(0, 0.1, 1000000))
+plt.figure(6)
+plt.plot(t, voStep)
+plt.title(r'Step Response of Highpass filter')
 plt.xlabel(r'$t\ \to$')
 plt.ylabel(r'$V_o(t)\ \to$')
+plt.xlim(0, 1e-3)
 plt.grid(True)
+plt.savefig(plotsDir+'Fig 6.png')
+
+vi = np.heaviside(t,1)*(np.sin(2e3*PI*t)+np.cos(2e6*PI*t))
+
+plt.figure(7)
+plt.plot(t, vi)
+plt.title(r'$V_i(t)=(sin(2x10^3\pi t)+cos(2x10^6\pi t))u(t)$ to Highpass filter')
+plt.xlabel(r'$t\ \to$')
+plt.ylabel(r'$V_i(t)\ \to$')
+plt.xlim(0, 1e-3)
+plt.grid(True)
+plt.savefig(plotsDir+'Fig 7.png')
+
+time, vOut, rest = sgnl.lsim(ckt2, vi, np.linspace(0, 1e-1, 1e6))
+plt.figure(8)
+plt.plot(time, vOut)
+plt.title(r'$V_o(t)$ for $V_i(t)=(sin(2x10^3\pi t)+cos(2x10^6\pi t))u(t)$ for Highpass filter')
+plt.xlabel(r'$t\ \to$')
+plt.ylabel(r'$V_o(t)\ \to$')
+plt.xlim(0, 1e-3)
+plt.grid(True)
+plt.savefig(plotsDir+'Fig 8.png')
+
 plt.show()
